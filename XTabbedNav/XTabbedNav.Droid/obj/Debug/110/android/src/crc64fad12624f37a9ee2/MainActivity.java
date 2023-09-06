0d16b6b9f3136071156141a9ef008cadbec32371
Nabeel Ahmad <69769575+thenabeelahmad@users.noreply.github.com>
2023-09-06 14:54:41 +0530

    Xamarin Tabbed Navigation

---
+++ XTabbedNav/XTabbedNav.Droid/obj/Debug/110/android/src/crc64fad12624f37a9ee2/MainActivity.java
@@ -0,0 +1,55 @@
+package crc64fad12624f37a9ee2;
+
+
+public class MainActivity
+	extends crc643f46942d9dd1fff9.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("XTabbedNav.Droid.MainActivity, XTabbedNav.Droid", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("XTabbedNav.Droid.MainActivity, XTabbedNav.Droid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("XTabbedNav.Droid.MainActivity, XTabbedNav.Droid", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ XTabbedNav/XTabbedNav/CarouselDemoPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XTabbedNav
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public class CarouselDemoPage : CarouselDemoPage
+    {
+        public CarouselDemoPage ()
+        {
+            Thickness padding;
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                case Device.Android:
+                    padding = new Thickness(0, 40, 0, 0);
+                    break;
+                default:
+                    padding = new Thickness();
+                    break;
+            }
+
+            var redContentPage = new ContentPage {
+                Padding = padding,
+                Content = new StackLayout {
+                    Children = {
+                        new Label {
+                            Text = "Red",
+                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center
+                        },
+                        new BoxView {
+                            Color = Color.Red,
+                            WidthRequest = 200,
+                            HeightRequest = 200,
+                            HorizontalOptions = LayoutOptions.Center,
+                            VerticalOptions = LayoutOptions.CenterAndExpand
+                        }
+                    }
+                }
+            };
+            var greenContentPage = new ContentPage {
+                Padding = padding,
+                Content = new StackLayout {
+                }
+            };
+            var blueContentPage = new ContentPage {
+                Padding = padding,
+                Content = new StackLayout {
+                }
+            };
+            Children.Add (redContentPage);
+            Children.Add (greenContentPage);
+            Children.Add (blueContentPage);
+        }
+    }
+}
